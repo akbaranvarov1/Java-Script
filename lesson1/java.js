608d69d0820eae4ca76a4e8becaa58faa4a8d996
akbaranvarov1 <akbaranvarovbk0805@gmail.com>
2023-02-15 19:06:19 +0500

    js

---
+++ lesson1/java.js
@@ -0,0 +1,70 @@
+// "use strict";
+// var number = 15
+// var number2 = 30
+// // var randomNumber = prompt("Raqam yozing")
+// // console.log(randomNumber);
+// // console.log(randomNumber * 5);
+// // var randomNumber = prompt("dollor hosiobi")
+// // console.log(randomNumber);
+// // console.log(randomNumber / 11700);
+// var randomNumber = prompt("dollor hosiobi")
+// console.log(randomNumber);
+// console.log(randomNumber * 11350);
+// // console.log(number + number2);
+// // console.log(number2 / number);
+// // console.log(number * number20);
+// let apple = "apple"
+// apple = "red"
+// // let apple = "iphone"
+// console.log(apple);
+// const box = "redmi note 9 pro"
+// console.log(box);
+// let eni = prompt('Tort burchka uzunligini aniqlang');
+// let boyi = prompt('Tort burchka boyini aniqlang');
+// let s = eni * boyi;
+// let p = 2 * (Number(eni) + Number(boyi));
+// alert(s);
+// alert(p);
+// console.log(eni);
+// console.log(boyi);
+
+
+
+// let number = prompt ("number enter")
+// let result = Math.pow(3,2)
+// alert(result);
+
+
+// let a=prompt('a somi kiriting');
+// let b=Math.PI('b somi kiriting');
+
+// alert(a - b)
+
+// console.log();
+
+// let q=prompt('nechi sonini darajaga kutarish kerak');
+// let w=prompt(' shu sonni nechinchi darajaga kutarish kerak');
+
+// alert(q**w)
+
+// let r=prompt('aylanani radiusini kiriting');
+
+
+// alert(Math.PI*r**2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
